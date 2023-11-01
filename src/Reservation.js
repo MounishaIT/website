b9c3935d3dbdd4d1072c817e9e60ececd87184a5
MounishaIT <118599104+MounishaIT@users.noreply.github.com>
2023-11-01 12:42:53 +0530

    Update Reservation.js

--- src/Reservation.js
+++ src/Reservation.js
@@ -1,7 +1,7 @@
-
 import React, { useState } from 'react';
 import './Reservation.css';
 import Navbar from './Navbar';
+
 function Reservation() {
   const containerStyle = {
     minHeight: '100vh',
@@ -11,6 +11,7 @@ function Reservation() {
     backgroundImage: `url('https://images.pexels.com/photos/313700/pexels-photo-313700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
     backgroundSize: 'cover',
   };
+
   const initialFormData = {
     name: '',
     email: '',
@@ -42,156 +43,147 @@ function Reservation() {
       return;
     }
 
-    fetch('/api/reservation', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setSubmissionMessage(data.message);
-
-        // Optionally, reset the form after successful submission
-        setFormData(initialFormData);
-      })
-      .catch((error) => console.error('Error:', error));
+    // Simulated server response (replace with actual API call)
+    // For demonstration purposes, we'll assume the server always succeeds
+    // In a real application, replace this with actual server communication
+    setTimeout(() => {
+      setSubmissionMessage('Reserved Successfully');
+      // Optionally, reset the form after successful submission
+      setFormData(initialFormData);
+    }, 1000);
   };
 
   return (
-    <div><Navbar/>
-    <div style={containerStyle}>
-      
-          <div className="reservation-form">
-            
-      <h1>Table Reservation</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+    <div>
+      <Navbar />
+      <div style={containerStyle}>
+        <div className="reservation-form">
+          <h1>Table Reservation</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Name:</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Date:</label>
+              <input
+                type="date"
+                name="date"
+                value={formData.date}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Time:</label>
+              <input
+                type="time"
+                name="time"
+                value={formData.time}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Number of Members:</label>
+              <input
+                type="number"
+                name="members"
+                value={formData.members}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Seating Preference:</label>
+              <select
+                name="preference"
+                value={formData.preference}
+                onChange={handleChange}
+              >
+                <option value="Indoor">Indoor</option>
+                <option value="Outdoor">Outdoor</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Phone Number:</label>
+              <input
+                type="tel"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Special Occasions:</label>
+              <select
+                name="specialOccasions"
+                value={formData.specialOccasions}
+                onChange={handleChange}
+              >
+                <option value="">Select</option>
+                <option value="Birthday">Birthday</option>
+                <option value="Anniversary">Anniversary</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Food Allergies:</label>
+              <select
+                name="allergies"
+                value={formData.allergies}
+                onChange={handleChange}
+              >
+                <option value="">Select</option>
+                <option value="None">None</option>
+                <option value="Gluten">Gluten</option>
+                <option value="Dairy">Dairy</option>
+                <option value="Nuts">Nuts</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label>Special Request:</label>
+              <textarea
+                name="specialRequest"
+                value={formData.specialRequest}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+            <button type="submit">Reserve</button>
+          </form>
+          {submissionMessage && (
+            <p className="submission-message">{submissionMessage}</p>
+          )}
         </div>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label>Date:</label>
-          <input
-            type="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Time:</label>
-          <input
-            type="time"
-            name="time"
-            value={formData.time}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Number of Members:</label>
-          <input
-            type="number"
-            name="members"
-            value={formData.members}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Seating Preference:</label>
-          <select
-            name="preference"
-            value={formData.preference}
-            onChange={handleChange}
-          >
-            <option value="Indoor">Indoor</option>
-            <option value="Outdoor">Outdoor</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Phone Number:</label>
-          <input
-            type="tel"
-            name="phoneNumber"
-            value={formData.phoneNumber}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Special Occasions:</label>
-          <select
-            name="specialOccasions"
-            value={formData.specialOccasions}
-            onChange={handleChange}
-          >
-            <option value="">Select</option>
-            <option value="Birthday">Birthday</option>
-            <option value="Anniversary">Anniversary</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Food Allergies:</label>
-          <select
-            name="allergies"
-            value={formData.allergies}
-            onChange={handleChange}
-          >
-            <option value="">Select</option>
-            <option value="None">None</option>
-            <option value="Gluten">Gluten</option>
-            <option value="Dairy">Dairy</option>
-            <option value="Nuts">Nuts</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Special Request:</label>
-          <textarea
-            name="specialRequest"
-            value={formData.specialRequest}
-            onChange={handleChange}
-          ></textarea>
-        </div>
-        <button type="submit">Reserve</button>
-      </form>
-      {submissionMessage && (
-        <p className="submission-message">{submissionMessage}</p>
-      )}
-    </div>
-    </div>
+      </div>
     </div>
   );
 }
